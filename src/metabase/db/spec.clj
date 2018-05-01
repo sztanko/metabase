c548e00f06660f2d037cba9f92597cecf2a018da
Demeter Sztanko <demeter.sztanko@revolut.com>
2018-05-01 11:23:00 +0100

    Added exasol driver

--- src/metabase/db/spec.clj
+++ src/metabase/db/spec.clj
@@ -38,3 +38,59 @@
           :subname (str "//" host ":" port "/" db)
           :delimiters "`"}
          (dissoc opts :host :port :db)))
+
+(defn exasol
+"Create a database specification for an Exasol database. Opts should include keys
+for :db, :user, and :password. You can also optionally set host and port.
+Delimiters are automatically set to \"`\"."
+[{:keys [host port db]
+  :or {host "localhost", port 8563, db ""}
+  :as opts}]
+(merge {:classname "com.exasol.jdbc.EXADriver" ; must be in classpath
+        :subprotocol "exa"
+        :subname (str host ":" port "/" db)}
+        (dissoc opts :host :port :db)))
+
+(defn exasol
+  "Create a database specification for an Exasol database. Opts should include keys
+  for :db, :user, and :password. You can also optionally set host and port.
+  Delimiters are automatically set to \"`\"."
+  [{:keys [host port db]
+    :or {host "localhost", port 8563, db ""}
+    :as opts}]
+  (merge {:classname "com.exasol.jdbc.EXADriver" ; must be in classpath
+          :subprotocol "exa"
+          :user "sys"
+          :password "exasol"
+          :subname (str host ":" port )} ; "?schema=" db)}
+          ; (dissoc opts :host :port :db)
+          ))
+
+(defn exasol_copy
+  "Create a database specification for an Exasol database. Opts should include keys
+  for :db, :user, and :password. You can also optionally set host and port.
+  Delimiters are automatically set to \"`\"."
+  [{:keys [host port db]
+    :or {host "localhost", port 8563, db "revolut-analytics"}
+    :as opts}]
+  (merge {:classname "com.exasol.jdbc.EXADriver" ; must be in classpath
+          :subprotocol "exa"
+          :user "sys"
+          :password "exasol"
+          :schema "revolut_analytics"
+          :subname (str host ":" port )} ; "?schema=" db)}
+          (dissoc opts :host :port :db)
+          ))
+
+(defn exasol_fake
+  "Create a database specification for a postgres database. Opts should include
+  keys for :db, :user, and :password. You can also optionally set host and
+  port."
+  [{:keys [host port db]
+    :or {host "localhost", port 5432, db ""}
+    :as opts}]
+  (merge {:classname "org.postgresql.Driver" ; must be in classpath
+          :subprotocol "postgresql"
+          :subname (str "//" host ":" port "/" db "?OpenSourceSubProtocolOverride=true")}
+            (dissoc opts :host :port :db)))
+        
---
+++ src/metabase/driver/exasol.clj
@@ -0,0 +1,150 @@
+(ns metabase.driver.exasol
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure
+            [set :as set :refer [rename-keys]]
+            [string :as s]]
+            [clojure.tools.logging :as log]
+            [honeysql.core :as hsql]
+            [metabase
+             [driver :as driver]
+             [util :as u]]
+            [metabase.db.spec :as dbspec] 
+            [metabase.driver.generic-sql :as sql]
+            [metabase.util
+             [honeysql-extensions :as hx]
+             [ssh :as ssh]]))
+
+(defrecord ExasolDriver []
+clojure.lang.Named
+(getName [_] "Exasol"))
+
+(def ^:private ^:const column->base-type
+  "Map of Exasol column types -> Field base types. Add more mappings here as you come across them."
+  {:BIGINT         :type/BigInteger
+   :BOOLEAN        :type/Boolean
+   :CHAR           :type/Text
+   :DATE           :type/Date
+   :DECIMAL        :type/Decimal
+   :FLOAT          :type/Float
+   :GEOMETRY       :type/Text
+   :INTEGER        :type/Integer
+   :SMALLINT       :type/Integer
+   :TIMESTAMP      :type/DateTime
+   :TINYINT        :type/Integer
+   :VARCHAR        :type/Text
+   (keyword "DOUBLE PRECISION")               :type/Float
+   (keyword "INTERVAL DAY TO SECOND")         :type/*
+   (keyword "INTERVAL YEAR TO MONTH")         :type/*
+   (keyword "LONG VARCHAR")                   :type/Text
+   (keyword "TIMESTAMP WITH LOCAL TIME ZONE") :type/DateTime})
+
+
+   (defn- connection-details->spec [{ssl? :ssl, :as details-map}]
+    (-> details-map
+        (update :port (fn [port]
+                        (if (string? port)
+                          (Integer/parseInt port)
+                          port)))
+        (dissoc :ssl)
+        (rename-keys {:dbname :db})
+        dbspec/exasol
+        (sql/handle-additional-options details-map)))
+
+(defn- unix-timestamp->timestamp [expr seconds-or-milliseconds]
+  (hsql/call :from_posix_time (case seconds-or-milliseconds
+                              :seconds      expr
+                              :milliseconds (hx// expr 1000))))
+
+
+(defn- date-trunc [unit expr] (hsql/call :date_trunc (hx/literal unit) (hx/->timestamp expr)))
+(defn- extract    [unit expr] (hsql/call :extract    unit              (hx/->timestamp expr)))
+
+(def ^:private extract-integer (comp hx/->integer extract))
+
+(def ^:private ^:const one-day (hsql/raw "INTERVAL '1 day'"))
+
+(defn- date [unit expr]
+  (case unit
+    :default         expr
+    :minute          (date-trunc :minute expr)
+    :minute-of-hour  (extract-integer :minute expr)
+    :hour            (date-trunc :hour expr)
+    :hour-of-day     (extract-integer :hour expr)
+    :day             (hx/->date expr)
+    ;; Postgres DOW is 0 (Sun) - 6 (Sat); increment this to be consistent with Java, H2, MySQL, and Mongo (1-7)
+    :day-of-week     (hx/inc (extract-integer :dow expr))
+    :day-of-month    (extract-integer :day expr)
+    :day-of-year     (extract-integer :doy expr)
+    ;; Postgres weeks start on Monday, so shift this date into the proper bucket and then decrement the resulting day
+    :week            (hx/- (date-trunc :week (hx/+ (hx/->timestamp expr) one-day))
+                           one-day)
+    :week-of-year    (extract-integer :week (hx/+ (hx/->timestamp expr) one-day))
+    :month           (date-trunc :month expr)
+    :month-of-year   (extract-integer :month expr)
+    :quarter         (date-trunc :quarter expr)
+    :quarter-of-year (extract-integer :quarter expr)
+    :year            (extract-integer :year expr)))
+
+(defn- date-interval [unit amount]
+  (hsql/raw (format "(NOW() + INTERVAL '%d %s')" (int amount) (name unit))))
+
+(defn- string-length-fn [field-key]
+  (hsql/call :char_length (hx/cast :Varchar field-key)))
+
+
+(def ^:private exasol-date-formatters (driver/create-db-time-formatters "yyyy-MM-dd HH:mm:ss"))
+(def ^:private exasol-db-time-query "select to_char(CURRENT_TIMESTAMP, 'YYYY-MM-DD HH24:MI:SS')")
+
+(u/strict-extend ExasolDriver
+  driver/IDriver
+  (merge (sql/IDriverSQLDefaultsMixin)
+         {:date-interval     (u/drop-first-arg date-interval)
+          :details-fields    (constantly (ssh/with-tunnel-config
+                                           [{:name         "host"
+                                             :display-name "Host"
+                                             :default      "localhost"}
+                                            {:name         "port"
+                                             :display-name "Port"
+                                             :type         :integer
+                                             :default      8563}
+                                            {:name         "dbname"
+                                             :display-name "Database name"
+                                             :placeholder  "birds_of_the_word"
+                                             :required     true}
+                                            {:name         "user"
+                                             :display-name "Database username"
+                                             :placeholder  "sys"
+                                             :required     true}
+                                            {:name         "password"
+                                             :display-name "Database password"
+                                             :type         :password
+                                             :placeholder  "exasol"}]))
+          :current-db-time   (driver/make-current-db-time-fn exasol-db-time-query exasol-date-formatters)
+;          :features                          (constantly (set/union #{:set-timezone
+;                                                                      :basic-aggregations
+;                                                                      :standard-deviation-aggregations
+;                                                                      :expressions
+;                                                                      :native-parameters
+;                                                                      :nested-queries
+;                                                                      :expression-aggregations
+;                                                                      :binning
+;                                                                      :native-query-params}))
+                                                                      })
+  sql/ISQLDriver
+  (merge (sql/ISQLDriverDefaultsMixin)
+         {:column->base-type         (u/drop-first-arg column->base-type)
+          :connection-details->spec  (u/drop-first-arg connection-details->spec)
+          :date                      (u/drop-first-arg date)
+          :set-timezone-sql          (constantly "ALTER SESSION SET time_zone = '%s';")
+          :string-length-fn          (u/drop-first-arg string-length-fn)
+          :unix-timestamp->timestamp (u/drop-first-arg unix-timestamp->timestamp)}))
+
+(defn -init-driver
+  "Register the Exasol driver when found on the classpath"
+  []
+  ;; only register the Exasol driver if the JDBC driver is available
+  (driver/register-driver! :exasol (ExasolDriver.)))
+;; only register the Exasol driver if the JDBC driver is available
+;; (when (u/ignore-exceptions
+;;  (Class/forName "com.exasol.jdbc.EXADriver"))
+;; (driver/register-driver! :exasol (ExasolDriver.))))
